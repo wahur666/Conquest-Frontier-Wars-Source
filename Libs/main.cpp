8bd851bbc28b2326d836b6acc474fb67b328cc14
Szecsodi Imre <imre.secodi@gmail.com>
2026-01-22 21:03:54 +0100

    remove rust from the project

--- Libs/main.cpp
+++ Libs/main.cpp
@@ -1,30 +1,12 @@
 ﻿#include <iostream>
 
 #include "DACOM.H"
-#include "IRustFunnyComponent.h"
 
 ICOManager *DACOM;
 
 int main() {
     DACOM = DACOM_Acquire();
-    DACOM->SetINIConfig("[Libraries]\r\ncom_rom_rs.dll", DACOM_INI_STRING);
-
-    ICOManager* dacom = DACOM_Acquire();
-    void* pFunny = nullptr;
-
-    DACOMDESC desc("IRustFunnyComponent");
-    dacom->CreateInstance(&desc, &pFunny);
-
-    IRustFunnyComponent* funny = (IRustFunnyComponent*)pFunny;
-    int number = funny->GetTheFunnyNumber();  // Returns 69
-    printf("Funny number: %d\n", number);
-
-    char buffer[256];
-    funny->ConcatenateStringWithInt("Hello", 42, buffer, sizeof(buffer));
-    printf("Result: %s\n", buffer);  // Prints "Hello 42"
-
-    funny->Release();
-
+    DACOM->SetINIConfig("[Libraries]\r\nDOSFile.dll", DACOM_INI_STRING);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
--- Libs/rust-components/com-rom-rs/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-//     println!(
-//         "cargo:rustc-link-search=D:\\git2\\Conquest-Frontier-Wars-Source\\Libs\\cmake-build-debug\\lib"
-//     );
-//     println!("cargo:rustc-link-lib=DACOM");
-}
